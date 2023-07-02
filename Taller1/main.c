edf861219f7399ea725a6953214bddf491f1e6f2
Andres Amaya <andres.amaya03@uptc.edu.co>
2023-07-01 22:46:01 -0500

    Menu de opciones creado

--- Taller1/main.c
+++ Taller1/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <ctype.h>
+
+
+
+void mainMenu(){
+
+    char *opciones=">>>>>MENU PRINCIPAL<<<<<\n\n"
+                   "\t1 - Numero Romano\n"
+                   "\t2 - Fatores Primos\n"
+                   "\t3 - Borrar Espacios\n"
+                   "\t4 - Numeros Egolatras\n"
+                   "\t5 - Numero Mágico\n"
+                   "\t6 - Fechas\n"
+                   "\t7 - Producto Punto\n"
+                   "\t8 - Multiplicación de Matrices\n"
+                   "\t9 - Matriz Mágica\n"
+                   "\tX - Salir\n";
+    char option;
+    do {
+        printf("%s",opciones);
+        scanf("%c",&option);
+
+        switch (option) {
+            case 1  :
+
+        }
+
+
+    }while (toupper(option)!='X');
+}
 
 int main() {
-    printf("PEPE GRILLO");
+   mainMenu();
     return 0;
 }
